c79fb3ae7ac3a695ba4464eb316646c0e794efa3
Abhicoding <shinganeabhishek@gmail.com>
2018-09-11 08:47:41 +0530

    create asset initialized

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var fs = require('fs')
-// var path = require('path')
+var path = require('path')
 var babylon = require('babylon')
 
+var entry = process.argv[2] || path.resolve(__dirname, 'Test/test.js')
+
 function createAsset (filePath) {
-  
+  var ast = babylon.parse(filePath)
+  console.log(createAsset)
 }
+
+createAsset(entry)
