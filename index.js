3372bbb86a4e7716dfe6eeabff0e1acf5e0113ba
Abhicoding <shinganeabhishek@gmail.com>
2018-09-11 17:58:12 +0530

    Done creating asset

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-var fs = require('fs')
-var path = require('path')
-var babylon = require('@babel/parser')
+const fs = require('fs')
+const path = require('path')
+// const babylon = require('@babel/parser')
+const babylon = require('@babel/core')
 
 var entry = process.argv[2] || path.resolve(__dirname, 'Test/test.js')
 
+var ID = 0
+
 function createAsset (filePath) {
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.log(err)
-    } else {
-      var ast = babylon.parse(data, {
-        sourceType: 'module'
-      })
-      console.log(ast)
+  var dependencies = []
+  
+  var data = fs.readFileSync(filePath, 'utf8')
+  
+  var ast = babylon.parse(data, {
+    sourceType: 'module'
+  })
+  
+  babylon.traverse(ast, {ImportDeclaration: ({node}) => {
+    dependencies.push(node.source.value)
     }
   })
+  
+  ID++
+  
+  var {code} = babylon.transformFromAst(ast) // This step is needed as transformed code has strict mode corrections
+  
+  return {id: ID, filePath, code, dependencies}
 }
 
-createAsset(entry)
+var mainAsset = createAsset(entry)
